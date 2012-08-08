0c75f7f61e1cd9ff8645af868b375418e4cbab4e
Faris Mohammed <farismosman@gmail.com>
2012-08-08 09:26:27 +0200

    1. you can view a class's ancestry with .ancestors 2. RuntimeError < Exception < Object

--- koans/about_exceptions.rb
+++ koans/about_exceptions.rb
@@ -5,11 +5,13 @@ class AboutExceptions < EdgeCase::Koan
   class MySpecialError < RuntimeError
   end
 
+  puts	MySpecialError.ancestors
+
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
